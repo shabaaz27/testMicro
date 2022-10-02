4751e87ea25e26064c45c7e65d70d5297927b358
shabaaz27 <khanshabaaz05@gmail.com>
2022-10-02 21:45:12 +0530

    comments test

---
+++ comments/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express()
+app.use(bodyParser.json())
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments',(req,res)=>{
+    console.log(req.params.id)
+     
+    if(commentsByPostId[req.params.id]){
+        return res.status(200).json({
+            commentsByPostId
+        })
+       
+    } else{
+        return res.status(404).json({
+            status:404,
+            message:"Not found"
+        })
+    }
+
+       
+})
+app.post('/posts/:id/comments',(req,res)=>{
+   const commentId = randomBytes(4).toString('hex') 
+    const {content} = req.body;
+
+    const comments = commentsByPostId[req.params.id] || []
+    
+    comments.push({id:commentId,content})
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).json({
+        comments
+    })  
+
+
+}) 
+
+app.listen(4001,()=>{
+    console.log('Comments service listening')
+})
+
