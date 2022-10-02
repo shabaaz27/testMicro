122481cce71e4383ce3099d8234293393bc4a5a8
shabaaz27 <khanshabaaz05@gmail.com>
2022-10-02 21:16:08 +0530

    Test with post req and res

--- posts/index.js
+++ posts/index.js
@@ -14,13 +14,20 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
-
+if(req.body.title){
   posts[id] = {
     id,
     title
   };
 
   res.status(201).send(posts[id]);
+}
+res.status(404).json({
+  status:404,
+  message:"please provide valid data"
+
+})
+
 });
 
 app.listen(4000, () => {
