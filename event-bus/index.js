27f220b5f4cdd59db477fb4e7eb9a11e4ae34ee6
shabaaz27 <khanshabaaz05@gmail.com>
2022-10-08 19:30:20 +0530

    Event-bus with all data stored sepreately

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,9 +6,13 @@ const app = express()
 
 app.use(bodyParser.json())
 
+const events = [];
+
+
+
 app.post('/events',(req,res)=>{
     const event = req.body;
-
+    events.push(event)
     axios.post('http://localhost:4000/events',event).catch(err=>console.log(err))
     axios.post('http://localhost:4001/events',event).catch(err=>console.log(err))
     axios.post('http://localhost:4002/events',event).catch(err=>console.log(err))
@@ -17,6 +21,9 @@ app.post('/events',(req,res)=>{
     res.send({status:'OK'})
 })
 
+app.get('/events',(req,res)=>{
+    res.send(events)
+})
 
 app.listen(4005,()=>{
     console.log('listening on 4005')
